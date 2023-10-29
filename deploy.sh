1daf4fe277908b0f2d28d22c0aacece979621073
Putselovsky Evgeniy <E9315717@yandex.ru>
2023-10-29 11:23:04 +0300

    Changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,7 @@ ssh -p 2425 root@10.0.2.15 /bin/bash <<EOF
   git commit -m "Local changes"
   git pull origin main
   cp -r Server/* /var/www/test_backend_app/CodeRoute/Server/
+  cd /etc/systemd/system
+  sudo systemctl restart kestrel-deploy-guide.service
   exit
 EOF
